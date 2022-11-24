b7000416940d4d1464ef29c98f04adb4d0dd5a87
Dante Silva <82445439+dantesjc@users.noreply.github.com>
2022-11-24 19:47:43 -0300

    Update readme.m

--- Engenharia III/Singleton/Pattern/readme.m
+++ Engenharia III/Singleton/Pattern/readme.m
@@ -1,1 +1,13 @@
+package pattern;
 
+public class App {
+
+	public static void main(String[] args) {
+        BancoDados primeiroBanco = BancoDados.getInstance();
+        
+        /* AINDA A MESMA INSTÂNCIA DE bd */
+        BancoDados novoBanco = BancoDados.getInstance();
+
+	}
+
+}
