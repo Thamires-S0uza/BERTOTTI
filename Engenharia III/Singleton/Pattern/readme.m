31c4319dee288aa94570c9c64ab1e4f1e080153b
Dante Silva <82445439+dantesjc@users.noreply.github.com>
2022-11-28 09:40:14 -0300

    Update readme.m

--- Engenharia III/Singleton/Pattern/readme.m
+++ Engenharia III/Singleton/Pattern/readme.m
@@ -7,7 +7,7 @@
 	public static void main(String[] args) {
         BancoDados primeiroBanco = BancoDados.getInstance();
         
-        /* AINDA A MESMA INSTÂNCIA DE bd */
+    
         BancoDados novoBanco = BancoDados.getInstance();
 
 	}
